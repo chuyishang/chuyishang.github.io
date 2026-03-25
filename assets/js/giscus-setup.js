1939f6325ee2af40140411cb8c8ba850bc743935
chuyishang <chuyishang@berkeley.edu>
2026-03-25 06:21:20 +0000

    Deploying to gh-pages from @ chuyishang/chuyishang.github.io@e4f06cc66a57fd71712e2a6001870cd78deebc9b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -19,9 +19,9 @@ function determineGiscusTheme() {
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
     "data-repo": "chuyishang/chuyishang.github.io",
-    "data-repo-id": "",
+    "data-repo-id": "R_kgDOQhbMFQ",
     "data-category": "Comments",
-    "data-category-id": "",
+    "data-category-id": "General",
     "data-mapping": "title",
     "data-strict": "1",
     "data-reactions-enabled": "1",
